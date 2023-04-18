97381c1d835f6b777a63b69061bbd8d44a1c35e2
AKASH SANJEEV <akashcsanjeev@gmail.com>
2023-04-18 22:35:46 +0530

    Routes added to map

--- lib/screens/beat_map.dart
+++ lib/screens/beat_map.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
@@ -26,8 +27,7 @@ class _UserMapState extends State<UserMap> {
   Set<Marker> _markers = {};
   int count = 0;
 
-  List<dynamic> routes = [];
-  List<LatLng> polylineLatLng = [];
+  List<dynamic> legs = [];
 
   @override
   void initState() {
@@ -90,19 +90,16 @@ class _UserMapState extends State<UserMap> {
     final response = await http.get(url);
     final body = response.body;
     final json = jsonDecode(body);
-    routes = json['routes'][0]['legs'][0]['steps'];
 
-    polylineLatLng.add(latlng[0]);
-    print(latlng[0]);
-    print(
-        '${routes[0]['end_location']['lat']} ${routes[0]['end_location']['lng']}');
-    for (var i = 0; i < routes.length; i++) {
-      polylineLatLng.add(LatLng(
-          routes[i]['end_location']['lat'], routes[i]['end_location']['lng']));
-    }
+    List<PointLatLng> pointss = PolylinePoints()
+        .decodePolyline(json['routes'][0]['overview_polyline']['points']);
 
-    polylines
-        .add(Polyline(polylineId: PolylineId("1"), points: polylineLatLng));
+    polylines.add(Polyline(
+        polylineId: PolylineId("1"),
+        points: pointss.map((e) => LatLng(e.latitude, e.longitude)).toList(),
+        color:
+            Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+        width: 4));
 
     setState(() {});
   }

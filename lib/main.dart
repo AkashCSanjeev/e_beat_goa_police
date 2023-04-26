1aaae4ba55f73aac767727380fd5203e2b13554d
AKASH SANJEEV <akashcsanjeev@gmail.com>
2023-04-27 03:23:11 +0530

    dependency added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_beat/screens/all_routes.dart';
 import 'package:e_beat/screens/create_route.dart';
 import 'package:e_beat/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
